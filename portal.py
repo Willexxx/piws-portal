1a4b67404426ba8ec0cb5c31a4375fdcc2b18a66
willexxx <william.ad.baird@gmail.com>
2021-11-10 08:50:19 +1100

    task: Charts

--- portal.py
+++ portal.py
@@ -1,6 +1,9 @@
+import datetime
 from flask import Flask
 from flask import render_template
+from flask import request
 import psycopg2
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -35,29 +38,43 @@ def temperature():
     cur.execute("select time, value from readings where type = 'temperature' order by time asc;")
     rows = cur.fetchall()
 
-    return render_template('data_page.html', rows=rows, page_title='Temperature', column_heading='Temperature (°C)')
+    return render_template('data_page.html', rows=rows, page_title='Temperature', column_heading='Temperature (°C)', type='temperature')
 
 
 @app.route('/humidity')
 def humidity():
     conn = psycopg2.connect("dbname=readings user=postgres")
     cur = conn.cursor()
-
     cur.execute("select time, value from readings where type = 'humidity' order by time asc;")
     rows = cur.fetchall()
 
-    return render_template('data_page.html', rows=rows, page_title='Humidity', column_heading='Humidity (g/m3)')
+    return render_template('data_page.html', rows=rows, page_title='Humidity', column_heading='Humidity (g/m3)', type='humidity')
 
 
 @app.route('/light_level')
 def light_level():
     conn = psycopg2.connect("dbname=readings user=postgres")
     cur = conn.cursor()
-
     cur.execute("select time, value from readings where type = 'light' order by time asc;")
     rows = cur.fetchall()
 
-    return render_template('data_page.html', rows=rows, page_title='Light Level', column_heading='Light (lumens)')
+    return render_template('data_page.html', rows=rows, page_title='Light Level', column_heading='Light (lumens)', type='light')
+
+
+@app.route('/graph')
+def graph_fragment():
+    the_type = request.args.get('type')
+    conn = psycopg2.connect("dbname=readings user=postgres")
+    cur = conn.cursor()
+    cur.execute("select time, value from readings where type = %s order by time asc;", (the_type,))
+    rows = cur.fetchall()
+    labels = []
+    values = []
+    for row in rows:
+        labels.append(row[0].strftime('%Y-%m-%d %H:%M'))
+        values.append(row[1])
+
+    return jsonify({"labels": labels, "datasets": [{"data": values, "label": the_type}]})
 
 
 app.run(host="0.0.0.0")
